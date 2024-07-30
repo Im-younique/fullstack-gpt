a322b9d4af5bc552515ac1cfd13740f57c9884db
임동윤 <dyyim4725@gmail.com>
2024-07-30 17:26:28 +0900

    [Add] Made Private GPT

--- pages/PrivateGPT.py
+++ pages/PrivateGPT.py
@@ -1,8 +1,151 @@
 import streamlit as st
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OllamaEmbeddings
+from langchain.vectorstores.faiss import FAISS
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.chat_models import ChatOllama
 
 st.set_page_config(
     page_title="PrivateGPT",
     page_icon="🤫"
 )
 
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, 'ai')
+
+    def on_llm_new_token(self, token, *arg, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+    
+
+llm = ChatOllama(
+    model="mistral:latest",
+    temperature=0.1,
+    streaming=True,
+    callbacks=[
+        ChatCallbackHandler(),
+    ]
+)
+
+llm_for_memory = ChatOllama(model="mistral:latest",temperature=0.1)
+
+
+@st.cache_resource
+def init_memory():
+    return ConversationSummaryBufferMemory(
+        llm=llm_for_memory,
+        max_token_limit=120,
+        memory_key="chat_history",
+        return_messages=True
+    )
+
+@st.cache_resource(show_spinner="Embedding file...")
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/private_files/{file.name}"
+    
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    cache_dir = LocalFileStore(f"./.cache/private_embeddings/{file.name}")
+
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100
+    )
+
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OllamaEmbeddings(
+        model="mistral:latest"
+    )
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vetorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vetorstore.as_retriever()
+    return retriever
+
+def save_message(message, role):
+    st.session_state["messages"].append({"message": message, "role": role})
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        save_message(message, role)
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+def load_memory(_):
+    return memory.load_memory_variables({})["chat_history"]
+
+memory = init_memory()
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """
+    Answer the question using ONLY the following context. If you don't know the answer
+    just say you don't know. DON'T make anything up.
+
+    Context: {context}
+    """),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("human", "{question}")
+])
+
 st.title("PrivateGPT")
+
+st.markdown("""
+Welcome!
+            
+Use this chatbot to ask question to an AI About your files!
+            
+Upload your file in the sidebar
+            
+Also, you can use this model in off-line!
+            
+You shold intall Oll
+""")
+
+with st.sidebar:
+    file = st.file_uploader("Upload a .txt .pdf of .docx file", type=["pdf", "txt", "docx"])
+
+if file:
+    retriever = embed_file(file)
+
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Send message to AI, whatever you have question about the file")
+    if message:
+        send_message(message, "human")
+        chain = {
+            "context": retriever | RunnableLambda(format_docs),
+            "chat_history": RunnableLambda(load_memory),
+            "question": RunnablePassthrough()
+        } | prompt | llm
+        with st.chat_message("ai"):
+            response = chain.invoke(message)
+        memory.save_context({"input": message}, {"output": response.content})
+
+        # docs = retriever.invoke(message)
+        # docs = "\n\n".join(document.page_content for document in docs)
+        # prompt = template.format_messages(context=docs, question=message)
+else:
+    st.session_state["messages"] = []
+    memory.clear()
